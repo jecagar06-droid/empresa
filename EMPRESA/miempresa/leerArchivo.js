32663994f49f7306aca93d3e7174eed71458a42b
jecagar06-droid <jecagar06@gmail.com>
2025-11-22 10:07:28 -0500

    Delete EMPRESA/miempresa directory

--- EMPRESA/miempresa/leerArchivo.js
+++
@@ -1,9 +0,0 @@
-// leerArchivo.js
-const fs = require("fs");
-
-function leer(archivo, encoding = "utf8") {
-    // Si el archivo no existe, lanzamos el error hacia quien llamó
-    return fs.readFileSync(archivo, encoding);
-}
-
-module.exports = { leer };
